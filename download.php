3cde5e9dd0a72e7e845f5cf4523687631d70c38e
Eliot Lear <lear@cisco.com>
2020-03-23 14:41:51 +0100

    base64 doesn't autodecode in ff.

--- download.php
+++ download.php
@@ -30,10 +30,10 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 if ( session_id() ) {
-  $output=$_SESSION['zipfile'];
+  $output=base64_decode($_SESSION['zipfile']);
   $model = $_SESSION['model'];
   header('Content-Type: application/zip');
-  header("Content-Transfer-Encoding: base64");
+  header("Content-Transfer-Encoding: binary");
   header("Content-disposition: attachment; filename=\"" . $model . ".zip\"");
   printf("%s",$output);
 } else {
