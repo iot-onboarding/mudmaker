670f1f21db785c7d7fd5440647c1dcaf5791037a
Eliot Lear <lear@cisco.com>
2024-12-20 12:27:03 +0100

    Initial revision of the UX

--- aux.js
+++ aux.js
@@ -240,3 +240,26 @@ function j2pp(b64) {
     xhr.send(jsonText);
 }
 
+//find all fieldsets marked as "toggleAble"
+$('fieldset.toggleable').each(function() {
+	var $fieldSet = $(this);
+	
+	//find the "legend"... if it does not exist then add it
+	var $theLegend = $fieldSet.find('> legend');
+	if ($theLegend.length === 0) {
+	  //add a legend if it does not exists and return reference to it
+	  $theLegend = $fieldSet.append('<legend>&nbsp;</legend>').find('> legend');
+	}
+	
+	$theLegend.click(function () {
+	  //when the legend gets clicked, add an "expanded" class to the parent container fieldset... css will take care of the rest in terms of displaying
+	  $(this).parent().toggleClass('expanded');
+	});
+	
+	if ($theLegend.find('> span').length == 0) {
+	  //there is no span... so take contents of legend and wrap it with a span... the CSS needs this
+	  $theLegend.html('<span>' + $theLegend.html() + '</span>');
+	}
+  });
+  
+  
