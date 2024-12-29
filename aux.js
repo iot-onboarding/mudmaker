e76cd8dfc23552dc7a8001f800cf01321c962241
Eliot Lear <lear@cisco.com>
2024-12-29 08:25:38 +0100

    remove unused js and css.

--- aux.js
+++ aux.js
@@ -249,34 +249,3 @@ function setboxes(idname) {
 	document.getElementById(idbox).checked = false;
     }
 }
-
-//find all fieldsets marked as "toggleAble"
-$('fieldset.toggleable').each(function() {
-	var $fieldSet = $(this);
-	
-	//find the "legend"... if it does not exist then add it
-	var $theLegend = $fieldSet.find('> legend');
-	if ($theLegend.length === 0) {
-	  //add a legend if it does not exists and return reference to it
-	  $theLegend = $fieldSet.append('<legend>&nbsp;</legend>').find('> legend');
-	}
-	
-	$theLegend.click(function () {
-	  //when the legend gets clicked, add an "expanded" class to the parent container fieldset... css will take care of the rest in terms of displaying
-	    p=$(this).parent();
-	    pid=p[0].id;
-  	    p.toggleClass('expanded');
-	    if (p[0].className == 'toggleable expanded') {
-		document.getElementById(pid + "box").checked = true;
-	    } else {
-		document.getElementById(pid + "box").checked = false;
-	    }
-	});
-	
-	if ($theLegend.find('> span').length == 0) {
-	  //there is no span... so take contents of legend and wrap it with a span... the CSS needs this
-	  $theLegend.html('<span>' + $theLegend.html() + '</span>');
-	}
-  });
-  
-  
