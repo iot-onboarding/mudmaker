0d7392cdbeb22c03427b539805dbad31cf0f637f
Eliot Lear <lear@cisco.com>
2025-01-14 14:37:04 +0100

    switch targets

--- mudjs.js
+++ mudjs.js
@@ -269,7 +269,7 @@ $(document).on('change','.addable',function(e){
 })
 
 $(document).on('change','.addbasics',function(e){
-	var cur=e.delegateTarget.activeElement;
+	var cur=e.currentTarget.activeElement;
 	if ( cur.name != 'email_addr') {
 		document.mudFile['ietf-mud:mud'][cur.name] = cur.value;
 		saveMUD();
