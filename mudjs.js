b21491d3699ef78189ca5fe5cfe7c1289821c722
Eliot Lear <lear@cisco.com>
2025-01-15 21:04:46 +0100

    revert

--- mudjs.js
+++ mudjs.js
@@ -279,7 +279,7 @@ $(document).on('change','.addbasics',function(e){
 })
 
 $(document).on('change','.sbomstuff',function(e){
-	var cur = e.delegateTarget.activeElement;
+	var cur = e.currentTarget;
 	var whichsbom = document.getElementById("sbom").value;
 
 	if (whichsbom == "none" ) {
