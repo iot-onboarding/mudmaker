031efd0f409f423deef33fc177abcc6766cd822b
Eliot Lear <lear@cisco.com>
2025-01-17 11:49:01 +0100

    more state cleanup

--- mudjs.js
+++ mudjs.js
@@ -276,9 +276,11 @@ $(document).on('change','.addbasics',function(e){
 	if ( cur.value == '') {
 		delete document.mudFile['ietf-mud:mud'][cur.name];
 	} else {
-		document.mudFile['ietf-mud:mud'][cur.name] = cur.value;
-		if (cur.name == 'mfg-name') {
-			fillpub(cur);
+		if ( cur.validity.isvalid ) {
+			document.mudFile['ietf-mud:mud'][cur.name] = cur.value;
+			if (cur.name == 'mfg-name') {
+				fillpub(cur);
+			}
 		}
 	}
 	saveMUD();
