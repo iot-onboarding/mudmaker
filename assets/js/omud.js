16eb1bf189a54b43c3c8c84036278b9719888163
Eliot Lear <lear@cisco.com>
2025-01-28 14:08:23 +0100

    second attempt

--- assets/js/omud.js
+++ assets/js/omud.js
@@ -4,7 +4,9 @@
 function oAuthMudFlow(){
     const redirect_uri="https://mudmaker.org/apis/oauth2/response";
     const client_id = "Ov23licSoRbhBHkeDqPJ";
-    const state = crypto.randomBytes(16).toString("hex");
+    const csrfkey = new Uint8Array(16);
+    self.crypto.getRandomValues(csrfkey);
+    const state = csrfkey.toHex();
     localStorage.setItem("latestCSRFToken", state);
     const link = `https://github.com/login/oauth/authorize?client_id=${client_id}&response_type=code&scope=repo&redirect_uri=${redirect_uri}/integrations/github/oauth2/callback&state=${state}`;
     window.location.assign(link);
