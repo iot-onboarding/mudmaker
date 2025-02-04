60b9f5700ad93f0a6f928a1042164580acbf90ba
Eliot Lear <lear@cisco.com>
2025-02-04 18:05:35 +0100

    change reference

--- assets/js/omud.js
+++ assets/js/omud.js
@@ -7,7 +7,7 @@ function oAuthP1(){
     self.crypto.getRandomValues(csrfkey);
     const state = csrfkey.toHex();
     localStorage.setItem("latestCSRFToken", state);
-    localStorage.setItem("email",document.getElementById("email_addr"))
+    localStorage.setItem("email",document.getElementById("email_addr").value)
     const link = `https://github.com/login/oauth/authorize?client_id=${client_id}&response_type=code&scope=repo&redirect_uri=${redirect_uri}&state=${state}`;
     window.location.assign(link);
 }
