d2c502ce94fa3314b0be3e8614ff160d988d3ffa
Eliot Lear <lear@cisco.com>
2025-02-05 14:51:59 +0100

    Revert "separate out some stuff and try more then.ing" Just too hard with apache at the moment.
    This reverts commit 46916887b6e3e57d52a9da917a8060140af34a5f.

--- assets/js/omud.js
+++ assets/js/omud.js
@@ -18,9 +18,6 @@ function oAuthP2(){
   
   let state = myURL.searchParams.get("state");
   let code = myURL.searchParams.get("code");
-  let mudFile = JSON.parse(sessionStorage.getItem("mudfile"));
-  let mudurl = mudFile["ietf-mud:mud"]["mud-url"];
-
   if (state != null &&  code != null) {
       // validate the state parameter
     if (state !== localStorage.getItem("latestCSRFToken")) {
@@ -30,68 +27,39 @@ function oAuthP2(){
     localStorage.removeItem("latestCSRFToken");
     // send the code to the backend
     email = localStorage.getItem("email")
-    let s = document.getElementById("status");
-    s.innerHTML = "Completing authorization...";
-    fetch("/gitShove/completeAuth",{
-      method: "POST",
-      body: JSON.stringify(
-        {
-          "mudurl" : mudurl,
-          "code" : code
-        }
-      )
+    jsonbody = {
+      mudFile : b64_encode(sessionStorage.getItem("mudfile")),
+      "code" : code,
+      "email" : email,
+      "next-redirect" : "https://" + window.location.hostname
+    };
+    fetch("/gitShovel", {
+      method : "POST",
+      body : JSON.stringify(jsonbody),
+      headers :{
+        "Content-type" : "application/json"
+      }
     })
     .then(response => {
-      if ( ! response.ok )
+      if ( ! response.ok ) {
         return response.text();
+      }
       return response.json();
     })
-    .then( jsonortext=> {
-      let s = document.getElementById("status");
-      if (typeof jsonortext != "object") {
-        s.innerHTML = s.innerHTML + "fail: " + jsonortext;
-        return null;
-      }
-      s.innerHTML = s.innerHTML + "[ok]<br>Starting the commit...";
-      token = jsonortext["token"];
-      jsonbody = {
-        "token" : token,
-        "mudFile"  : b64_encode(mudFile),
-        "email" : email,
-        "next-redirect" : "https://" + window.location.hostname
-      };
-      return fetch("/gitShovel", {
-        method : "POST",
-        body : JSON.stringify(jsonbody),
-        headers :{
-          "Content-type" : "application/json"
-        }
-      })
-      .then(response => {
-        if ( response == null ){
-          return; // error, fetch wasn't called.
-        }
-        if (! response.ok ) {
-          return response.text();
-        }
-        return response.json();
-      })
-      .then ( jsonortext => {
-        let s = document.getElementById("two");
-        if (typeof jsonortext == "object") {
-          let user = jsonortext['user'];
-          let innerhtml = '<h2>Yay!  Your PR has been created</h2><p>You can click ' +
-            '<a href="https://github.com/' + user + '/mudfiles">here</a> to take you' +
-            'to your repo, which is ' + user + '/mudfiles.</p><h2>Next Steps</h2>' +
-            '<p>Next someone will review your PR.  You will see git notivations to' +
-            'as it is evaluated.</p>';
+    .then ( jsonortext => {
+      let s = document.getElementById("two");
+      if (typeof jsonortext == "object") {
+        let user = jsonortext['user'];
+        let innerhtml = '<h2>Yay!  Your PR has been created</h2><p>You can click' +
+          '<a href="https://github.com/' + user + '/mudfiles">here</a> to take you' +
+          'to your repo, which is ' + user + '/mudfiles.</p>';
 
-          s.innerHTML = innerhtml;
-          return;
-        }
-        s.innerHTML = jsonortext;
+        s.innerHTML = innerhtml;
+        return;
       }
-    )});
+      s.innerHTML = jsonortext;
+    }
+  );
     return;
   }
   document.getElementById("one").style.visibility = "inherit";
