f87fc1908fe73d602752d377561e67962c67c325
Eliot Lear <lear@cisco.com>
2025-02-10 22:22:49 +0100

    open summary when filled

--- assets/js/mudmaker.js
+++ assets/js/mudmaker.js
@@ -408,6 +408,7 @@ function reloadFields(){
 				if (typeof ace['matches']["ietf-mud:mud"] != 'undefined'){
 					for (let val in mudtypes ) {
 						if ( typeof ace['matches']["ietf-mud:mud"][mudtypes[val]] != 'undefined') {
+							document.getElementById(val).open = true;
 							nextAce = findNextAce(val);
 							if (! nextAce.children[0].readOnly) {
 								nextAce.children[0].value = ace['matches']["ietf-mud:mud"][mudtypes[va]];
